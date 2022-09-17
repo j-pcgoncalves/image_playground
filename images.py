4e27ad266b6fd60827b14b1bd03f2d9c83294a70
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-17 21:54:32 +0100

    creating a selfie image thumbnail

--- images.py
+++ images.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./selfie.jpg')
-new_img = img.resize((400, 400))
-new_img.save('thumbnail.jpg')
+img.thumbnail((400, 200))
+img.save('thumbnail.jpg')
 
 
