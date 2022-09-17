e031069ed98181a2d7e78fbdad6dcd4217e62af7
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-17 21:44:01 +0100

    resize black and white pikachu image

--- images.py
+++ images.py
@@ -3,5 +3,5 @@ from PIL import Image, ImageFilter
 img = Image.open('./pokedex/pikachu.jpg')
 filtered_img = img.convert('L')
 
-crooked = filtered_img.rotate(90)
-crooked.save('grey.png', 'png')
+resize = filtered_img.resize((300, 300))
+resize.save('grey.png', 'png')
