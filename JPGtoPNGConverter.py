734ac15bbf3f56e024556600c545adcf34f13234
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-17 22:07:20 +0100

    adding new folder

--- JPGtoPNGConverter.py
+++ JPGtoPNGConverter.py
@@ -7,6 +7,9 @@ image_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
 # check if new/ exists, if not create it
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
 
 # loop though Pokedex,
 # convert images to png
