934f4a32fa7cbdeef9ed8c4586b9607cb4098c72
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-17 21:39:17 +0100

    add grayscale to pikachu image

--- images.py
+++ images.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./pokedex/pikachu.jpg')
-filtered_img = img.filter(ImageFilter.SHARPEN)
+filtered_img = img.convert('L')
 
-filtered_img.save('blur.png', 'png')
+filtered_img.save('grey.png', 'png')
