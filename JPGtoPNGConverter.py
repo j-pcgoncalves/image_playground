755b88a88be5697098bbb143ad879ac47cc217a9
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-09-17 22:05:16 +0100

    grab first and second argument

---
+++ JPGtoPNGConverter.py
@@ -0,0 +1,13 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# check if new/ exists, if not create it
+
+# loop though Pokedex,
+# convert images to png
+# save them to the new folder
